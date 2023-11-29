f40951aacf24f1457122fa2cfc07ad7107299f6a
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-29 10:50:20 +0000

    [add]slide機能

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,2 +1,33 @@
-let title = "javascriptが使えました";
-alert(title);
+/* global $*/
+// let title = "javascriptが使えました";
+// alert(title);
+
+// $(document).ready(function(){
+//     $('.jquery').on('click', function(){
+//         $(this).css('color', 'red');
+//     });
+// });
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
+
+// swiperのオプションを定数化
+const opt = {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+}
+
+// swiperを実行（初期化）
+$(document).on('turbolinks:load', function(){
+// document(つまり全体)にjsを働かせるための記述。turbolinks:がないと('load'のみだと)読み込み時か働かず、ページ遷移などをすると効かなくなる。
+    let swiper = new Swiper('.swiper',opt);
+});
